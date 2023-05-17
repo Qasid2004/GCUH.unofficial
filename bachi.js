d43a960cdfeee0354ff16c6657ed1fa789aeb4f9
Qasid2004 <133877556+Qasid2004@users.noreply.github.com>
2023-05-17 17:38:28 +0500

    Add files via upload

---
+++ bachi.js
@@ -0,0 +1,6 @@
+document.getElementById("rate").onclick=function(){
+    document.getElementById("label").innerHTML = "Abhi bhi Sharam nhi Ayi";  
+}
+document.getElementById("rate").ondblclick=function(){
+document.getElementById("label2").innerHTML = "Lanat ha";
+}
