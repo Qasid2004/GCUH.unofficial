697c66091562848e5c939067ca020b083e25f4b7
Qasid2004 <133877556+Qasid2004@users.noreply.github.com>
2023-05-19 14:14:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+document.getElementById("bba").onclick = function(){
+document.getElementById("bba").innerHTML = "Tu Single hi Marega";
+}
+document.getElementById("botany").onclick = function(){
+document.getElementById("botany").innerHTML = "Woman";
+}
+document.getElementById("cs").onclick = function(){
+document.getElementById("cs").innerHTML = "Tu is baar bhi double bed pe single hi soega";
+}
+document.getElementById("chemistry").onclick = function(){
+    document.getElementById("chemistry").innerHTML = "Aaaahhhh... Wire";
+}
+document.getElementById("english").onclick = function(){
+document.getElementById("english").innerHTML = "Sir i dont know ke double sawari lag gai ha";
+}
+document.getElementById("education").onclick = function(){
+document.getElementById("education").innerHTML = "Saale is se acha to Commerce karleta";
+}
+document.getElementById("it").onclick = function(){
+    document.getElementById("it").innerHTML = "Phada kare kia ?";
+}
+    document.getElementById("maths").onclick = function(){
+        document.getElementById("maths").innerHTML = "Tera to case hi alag ha ?";
+    }
+        document.getElementById("physics").onclick = function(){
+            document.getElementById("physics").innerHTML = "Fees ko Crypto me lagade";
+        }
+        document.getElementById("advice").onclick = function(){
+            document.getElementById("advice").innerHTML = "Pyaar Muhabbat Dhoka ha Abhi Parhlo mauka ha";
+
+        }
+        document.getElementById("contact").onclick = function(){
+            window.alert("Ph : 112");
+        }
+
+
